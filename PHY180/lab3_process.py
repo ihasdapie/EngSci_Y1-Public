3696aa43cf992b444cbfbec2023652d5f9d67e4a
Brian Chen <brianchen.chen@mail.utoronto.ca>
2020-10-23 14:30:52 -0400

    oct 23 2020

--- PHY180/lab3_process.py
+++ PHY180/lab3_process.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python
-
-# you will need numpy, scipy, matplotlib. Either pip install or use anaconda
 import numpy as np 
 from scipy.signal import find_peaks
 import matplotlib.pyplot as plt
@@ -12,6 +10,10 @@ f = open('finaldata.csv') # your data file name
 # I used ", " as a delimiter, if you used spaces or anything
 # just change it in the .split() on line 15 to whatever you used
 # amp. doesn't have to be in radians but should cross the x-axis
+
+amp_err = 0.0117453 # 1 degree
+time_err = 0.016666 # 1 frame
+
 
 b = f.readlines()
 b = [i.strip().split(', ') for i in b]
@@ -25,7 +27,7 @@ nbt = nb.transpose()
 
 # find positive peaks(pp), negative peaks
 pp = find_peaks(nbt[1], prominence=0.3)[0]
-pn = find_peaks(nbt[1]*-1, prominence=0.3)[0]
+pn = find_peaks(nbt[1]*-1, prominence=0.3)[0] # you may have to change the prominence values depending on your setup. .3 should work for most
 peaks = np.concatenate((pp, pn), axis=0)
 
 
@@ -43,30 +45,37 @@ plt.show()
 
 # for maxima
 p_amp = nbt[1][pp][:len(pp)-1]
-period = nbt[0][pp]
-period = [ period[i]-period[i-1] for i in range(1, len(period))]
+p_period = nbt[0][pp]
+p_period = np.array([p_period[i]-p_period[i-1] for i in range(1, len(p_period))])
+p_time_err = np.array([time_err for i in range(len(p_amp))]) # this is ugly
+p_amp_err = np.array([amp_err for i in range(len(p_period))])
 
 
 # for minima
 n_amp = nbt[1][pn][:len(pn)-1]
-period = nbt[0][pn]
-period = [ period[i]-period[i-1] for i in range(1, len(period))]
+n_period = nbt[0][pn]
+n_period = np.array([n_period[i]-n_period[i-1] for i in range(1, len(n_period))])
 
-# plot period vs amp for maxima
-plt.plot(p_amp, period, 'ro')
 
+n_time_err = np.array([time_err for i in range(len(n_amp))]) # this is ugly
+n_amp_err = np.array([amp_err for i in range(len(n_period))])
+
+# plot period vs amp for minima 
+plt.errorbar(n_amp, n_period, yerr=  n_time_err, xerr = n_amp_err, fmt='.')
 plt.xlabel("Amplitude, θ")
 plt.ylabel("Period, T")
 plt.title("Period vs Amplitude")
 plt.show()
 
 
-# plot period vs amp for minima
-plt.plot(n_amp, period, 'ro')
+# plot period vs amp for maxima
+plt.errorbar(p_amp, p_period, yerr=  p_time_err, xerr = p_amp_err, fmt='.')
 plt.xlabel("Amplitude, θ")
-plt.ylabel("Period,T")
+plt.ylabel("Period, T")
 plt.title("Period vs Amplitude")
 plt.show()
+
+
 
 
 # Note that if you used tracker data and had time in (1/60)s like me, 
